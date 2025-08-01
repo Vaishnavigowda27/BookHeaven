63a2b6ae566f22623662900880245e2b04ac03fe
Vaishnavi S <150419017+Vaishnavigowda27@users.noreply.github.com>
2025-08-01 19:53:24 +0530

    Initial commit

---
+++ BookHeaven-Server-main/BookHeaven-Server-main/conn/conn.js
@@ -0,0 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const conn = async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}`);
+//     console.log("Connected to DB");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// conn();
+
+const mongoose = require("mongoose");
+
+const conn = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn();
